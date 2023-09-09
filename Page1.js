9a50de00a7b1629cce7b3d950105c5be22bfe707
StevensCortes <139996841+StevensCortes@users.noreply.github.com>
2023-09-08 20:11:16 -0500

    Add files via upload

---
+++ Page1.js
@@ -0,0 +1,29 @@
+
+function ImageHover(Sectionimagen){
+
+    if(Sectionimagen.style.height < 500){
+        Sectionimagen.style.height = "50%";
+      }
+      else {
+        Sectionimagen.style.width = "250%";
+      }
+    
+}
+function ImageHoverOut(Sectionimagen) {
+      Sectionimagen.style.width = "100%"
+    }
+
+    function iyu3Pheu(x) {
+        if(x.style.width < 500){
+          x.style.width = "50%";
+        }
+        else {
+          x.style.width = "100%";
+        }
+      }
+      function Xio8Hogh(x) {
+        x.style.width = "300px";
+      }
+    
+
+    
